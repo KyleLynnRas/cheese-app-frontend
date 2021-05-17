7f1742106a0e1d3ee85ab06139674f0e138d3c12
KyleLynnRas <kylelynn.ras@gmail.com>
2021-05-17 01:26:52 -0400

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 //Components 
 import Header from "./components/Header"
 import Main from "./components/Main"
+//Bulma
+import {Footer} from "react-bulma-components"
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer className="footer"/>
     </div>
   );
 }
